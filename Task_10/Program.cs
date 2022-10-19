dd8ebae8ebc39dd437fe1c05151acbcc9a2f242b
Aleksandr <alexandr.igig@gmail.com>
2022-10-20 00:41:50 +0300

    Add comment

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -10,3 +10,4 @@ int number1 = (number/10)-(number/100)*10;
 Console.WriteLine($"Исходное число {number}");
 Console.WriteLine($"Результат {number1}");
 
+// Как можно иначе записать 9 строку? (если можно)Ф
